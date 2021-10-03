4ab475c43387d6699bba441b94494be51a7a80e6
Danka02 <kamikoto02@mail.ru>
2021-10-03 19:36:55 +0300

    k

--- src/hw3.kt
+++ src/hw3.kt
@@ -1,6 +1,6 @@
 fun main() {
     val publishedAgo = 1000
-    val Min = if (publishedAgo > 3599) {
+    val min = if (publishedAgo > 3599) {
         publishedAgo / 3600
     } else {
         publishedAgo / 60
@@ -8,10 +8,10 @@ fun main() {
     val verdict = when (publishedAgo) {
         in 0..59 -> "менее минуты назад"
         in 60..179 -> "минуту назад"
-        in 180..299 -> "$Min минуты назад"
-        in 300..3599 -> "$Min минут назад"
-        in 3600..17999 -> "$Min часа назад"
-        else -> "$Min часов назад "
+        in 180..299 -> "$min минуты назад"
+        in 300..3599 -> "$min минут назад"
+        in 3600..17999 -> "$min часа назад"
+        else -> "$min часов назад "
     }
     println(verdict)
 }
